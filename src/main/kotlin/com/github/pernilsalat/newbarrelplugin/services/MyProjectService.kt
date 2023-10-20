65da900be6b490b8280daef563ff48ae186ebb4a
GitHub Action <action@github.com>
2023-10-20 10:02:24 +0000

    Template cleanup

--- src/main/kotlin/com/github/pernilsalat/newbarrelplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/pernilsalat/newbarrelplugin/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.pernilsalat.newbarrelplugin.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.pernilsalat.newbarrelplugin.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
